64e0b589ddf8d05358d2c9fb11b293f79fe2a445
gaborod <gaborod@gmail.com>
2025-03-09 08:22:43 -0500

    antiguo2

---
+++ antiguo/simulador/app.js
@@ -0,0 +1,11 @@
+
+const root = document.getElementById('root')
+
+render()
+
+function render(){
+    let tit = document.createElement('h1')
+    tit.innerText = "Simulación de cambio de Pensum"
+    root.appendChild(tit)
+
+}
